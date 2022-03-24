5cc97a597f1f542f59e19a31f3a90b5eae618a5c
AbdouTlili <ha_tlili@esi.dz>
2022-03-24 14:59:51 +0100

    pre-build of the e2sm_rmet plugin

---
+++ servicemodels/e2sm_rmet/go.mod
@@ -0,0 +1,3 @@
+module github.com/AbdouTlili/onos-e2-sm/servicemodels/e2smkpmv2
+
+go 1.16
---
+++ servicemodels/e2sm_rmet/main.go
@@ -0,0 +1,6 @@
+package main
+
+import "github.com/AbdouTlili/onos-e2-sm/servicemodels/e2sm_rmet/servicemodel"
+
+// ServiceModel is the exported symbol that gives an entry point to this shared module
+var ServiceModel servicemodel.RmetServiceModel
